a21f36d40e20002fea186aa2f5958a3282088426
AOA <aoa@d.co>
2025-09-25 11:26:13 +0300

    1.18.2 commit

--- src/main/java/net/alminoris/almirisweapons/AlmirisWeapons.java
+++ src/main/java/net/alminoris/almirisweapons/AlmirisWeapons.java
@@ -5,10 +5,7 @@ import net.alminoris.almirisweapons.entity.ModEntities;
 import net.alminoris.almirisweapons.entity.client.projectile.BulletEntityRenderer;
 import net.alminoris.almirisweapons.item.ModItems;
 import net.alminoris.almirisweapons.sound.ModSounds;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -54,19 +51,6 @@ public class AlmirisWeapons {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             EntityRenderers.register(ModEntities.BULLET.get(), BulletEntityRenderer::new);
-
-            event.enqueueWork(() -> {
-                Minecraft mc = Minecraft.getInstance();
-
-                ModelResourceLocation arquebusModel = new ModelResourceLocation(
-                        ResourceLocation.fromNamespaceAndPath(AlmirisWeapons.MOD_ID, "arquebus_3d"), "inventory");
-
-                ModelResourceLocation blunderbussModel = new ModelResourceLocation(
-                        ResourceLocation.fromNamespaceAndPath(AlmirisWeapons.MOD_ID, "blunderbuss_3d"), "inventory");
-
-                mc.getItemRenderer().getItemModelShaper().register(ModItems.ARQUEBUS.get(), arquebusModel);
-                mc.getItemRenderer().getItemModelShaper().register(ModItems.BLUNDERBUSS.get(), blunderbussModel);
-            });
         }
     }
 }
